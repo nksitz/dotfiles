4da6601e74d90c80492a4efcc58a3725ad9bd9b9
Nicolò Sitzia <nksitz@gmail.com>
2026-05-10 18:14:49 +1000

    Add aerospace and karabiner

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,17 @@ vim.opt.runtimepath:append("~/.local/share/nvim/site")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = false
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	},
+}
 
 -- Options
 vim.o.number = true
